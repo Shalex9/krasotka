895903b8ca6935131b69fcc34f66caa6376d79ca
Shevchuk Alexander <shalex9@gmail.com>
2016-12-30 00:29:38 +0200

    contacts

---
+++ dist/js/parallax.js
@@ -0,0 +1,20 @@
+(function () {
+      "use strict";
+
+      jQuery(window).bind('load', function () {
+
+            if (!device.tablet() && !device.mobile()) {
+
+                  /* if non-mobile device is detected*/
+                  // Parallax Init
+                  jQuery('.parallax').each(function () {
+                        jQuery(this).parallax('60%', 0.5, true);
+                  });
+            }
+            //  else {
+            //
+            //     /* if mobile device is detected*/
+            //     jQuery('.devider-section').addClass('parallax-off');
+            // }
+      });
+})(jQuery);
